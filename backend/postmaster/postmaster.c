7f59562be3afdaa8c7b674ddff365365062f94d6
liushige <liushige@mail.ustc.edu.cn>
2022-03-29 14:46:56 +0800

    Copy pg_xact

--- backend/postmaster/postmaster.c
+++ backend/postmaster/postmaster.c
@@ -6754,4 +6754,5 @@ rpc_init_file(char * db_dir_raw)
 	rpcinitfile(configdir, "global/pg_control");
 	rpcinitfile(configdir, "global/pg_control/pg_filenode.map");
 	rpcinitfile(configdir, "global/pg_syncconf");
+	rpcinitfile(configdir, "pg_xact/0000");
 }
